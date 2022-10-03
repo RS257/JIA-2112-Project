9d437f8bdeb13815c67980930429a6fafae40757
voytkam4 <112191624+voytkam4@users.noreply.github.com>
2022-10-03 18:25:35 -0400

    0.2.0 Commit (#27)
    Co-authored-by: Andrey Voytko <89618654+AndreyVoytko@users.noreply.github.com>

---
+++ tracker/users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('first_name','last_name', 'username', 'email', 'password1' ,'password2' )
