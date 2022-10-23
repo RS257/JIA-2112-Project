00babaa021b87f4b6793c5d0a91d34f22e163671
Andrey Voytko <89618654+AndreyVoytko@users.noreply.github.com>
2022-10-23 19:37:24 -0400

    0.2.2 Commit

--- tracker/users/forms.py
+++ tracker/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Images, Certificate
+from .models import Images, Certificate, Profile
 
 #Registartion form to render in html
 class UserForm(UserCreationForm):
@@ -13,9 +13,13 @@ class UserForm(UserCreationForm):
         model = User
         fields = ('first_name','last_name', 'username', 'email', 'password1' ,'password2' )
 
+class DateInput(forms.DateInput):
+    input_type = 'date'        
+
 #Upload form to render in html
 class ImageForm(forms.ModelForm):
     """Form for the image model"""
     class Meta:
         model = Images
-        fields = ('image_name', 'images')
+        widgets = {'certification_completion_date': DateInput()}
+        fields = ('certificate', 'certification_completion_date', 'file')
